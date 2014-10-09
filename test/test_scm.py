1a11dfce442bfae02452538713824b1f5485437e
Geoffrey Squire <geoffrey.squire@csiro.au>
2014-10-10 10:22:37 +1100

    Test skeleton, data, examples

---
+++ test/test_scm.py
@@ -0,0 +1,17 @@
+import os
+import scm
+import unittest
+import tempfile
+
+
+class ScmTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
