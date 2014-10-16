9f0919ca99ba6a952a1566de300b3fbe4eb3bfe1
Geoffrey Squire <geoffrey.squire@csiro.au>
2014-10-16 16:50:36 +1100

    Added ANUGA toolbox/template

--- test/data/populatedb.sh
+++ test/data/populatedb.sh
@@ -10,3 +10,11 @@ echo "Inserted TCRM toolbox: $tboxid"
 templateid=$(curl "$1/templates?tbox=$tboxid" -d @tcrmtemplate.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted TCRM template: $templateid"
+
+tboxid=$(curl "$1/toolboxes" -d @anuga-toolbox.json -X POST -H "Content-Type: application/json")
+
+echo "Inserted ANUGA toolbox: $tboxid"
+
+templateid=$(curl "$1/templates?tbox=$tboxid" -d @anuga-template.json -X POST -H "Content-Type: application/json")
+
+echo "Inserted ANUGA template: $templateid"
