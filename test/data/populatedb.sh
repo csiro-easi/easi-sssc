c443d1bcf603d5e3bea3cc615b0a906b6fcb949a
Geoffrey Squire <geoffrey.squire@csiro.au>
2014-10-22 10:57:44 +1100

    Renamed template -> solution

--- test/data/populatedb.sh
+++ test/data/populatedb.sh
@@ -3,11 +3,11 @@
 # Pass the URL of the service as the first parameter
 url="$1"
 
-tboxid=$(curl "$1/toolboxes" -d @tcrmbox.json -X POST -H "Content-Type: application/json")
+tboxid=$(curl "$1/toolboxes" -d @tcrm-toolbox.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted TCRM toolbox: $tboxid"
 
-solutionid=$(curl "$1/solutions?tbox=$tboxid" -d @tcrmsolution.json -X POST -H "Content-Type: application/json")
+solutionid=$(curl "$1/solutions?tbox=$tboxid" -d @tcrm-solution.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted TCRM solution: $solutionid"
 
