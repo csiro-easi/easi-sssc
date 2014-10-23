4868c23344bbed5f42c21f8ceb233ed230563688
Geoffrey Squire <geoffrey.squire@csiro.au>
2014-10-23 16:41:53 +1100

    Fix solutions, populate problems

--- test/data/populatedb.sh
+++ test/data/populatedb.sh
@@ -3,18 +3,26 @@
 # Pass the URL of the service as the first parameter
 url="$1"
 
+probid=$(curl "$1/problems" -d @tcrm-problem.json -X POST -H "Content-Type: application/json")
+
+echo "Inserted TCRM problem: $probid"
+
 tboxid=$(curl "$1/toolboxes" -d @tcrm-toolbox.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted TCRM toolbox: $tboxid"
 
-solutionid=$(curl "$1/solutions?tbox=$tboxid" -d @tcrm-solution.json -X POST -H "Content-Type: application/json")
+solutionid=$(curl "$1/solutions?tbox=$tboxid&problem=$probid" -d @tcrm-solution.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted TCRM solution: $solutionid"
+
+probid=$(curl "$1/problems" -d @anuga-problem.json -X POST -H "Content-Type: application/json")
+
+echo "Inserted ANUGA problem: $probid"
 
 tboxid=$(curl "$1/toolboxes" -d @anuga-toolbox.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted ANUGA toolbox: $tboxid"
 
-solutionid=$(curl "$1/solutions?tbox=$tboxid" -d @anuga-solution.json -X POST -H "Content-Type: application/json")
+solutionid=$(curl "$1/solutions?tbox=$tboxid&problem=$probid" -d @anuga-solution.json -X POST -H "Content-Type: application/json")
 
 echo "Inserted ANUGA solution: $solutionid"
