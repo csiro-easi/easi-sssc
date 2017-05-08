695a9878864413f8b1bb73dc4894080852a14f9e
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2017-05-08 16:57:00 +1000

    Can now load private keys and sign an entry

--- signingtool.py
+++ signingtool.py
@@ -1,7 +1,15 @@
 import argparse
 import json
-from views import hash_str, hash_dict
+from views import hash_dict
+from rsa import PrivateKey
+import rsa
+from datetime import datetime
 
+#
+# Key Pair creation:
+# Private key: openssl genrsa -out rsakey.pem 2048
+# Public  key: openssl rsa -in rsakey.pem -pubout > rsakey.pub.pem
+#
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Sign a SSSC entry")
@@ -14,11 +22,13 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--key', action='store',
                         help="Filename of the file containing the private signature key", required=True)
     parser.add_argument('-e', '--sourceurl', action='store',
-                        help="URL pointing to the entry that us to be signed", required=True)
+                        help="URL pointing to the entry that is to be signed", required=True)
     parser.add_argument('-s', '--signatureurl', action='store',
                         help="URL poiting to the destination where the signature will be submitted to", required=True)
 
     args = parser.parse_args()
+
+    entry_hash=None
 
     with open(args.filename, "r") as entry_file:
         entry_str = entry_file.read()
@@ -26,3 +36,16 @@ if __name__ == '__main__':
         entry_hash = hash_dict(entry_dict, log_file="C:\\opt\\sign_entry.json")
         print(entry_hash)
 
+    private_key = None
+
+    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+    signable_str = entry_hash+"$"+now
+
+    with open(args.key, "r") as key_file:
+        key_str = key_file.read()
+        private_key = PrivateKey.load_pkcs1(key_str)
+
+    signature  = rsa.sign(signable_str.encode("UTF-8"), private_key, "SHA-256")
+
+    print("end")
