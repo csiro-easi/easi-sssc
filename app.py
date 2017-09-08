c9e800c0aa23b0dc5965fa30d01ff201c488bba5
Geoffrey Squire <squ022@triplewood-bm.nexus.csiro.au>
2017-09-08 16:27:56 +1000

    Override default config via file in env var

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
+
+# Load default settings from local config file, then override values from
+# config file passed in the environment.
 app.config.from_pyfile('scm.config')
+loaded = app.config.from_envvar('SSSC_CONFIG', silent=True)
+if not loaded:
+    import os
+    cfile = os.environ.get('SSSC_CONFIG')
+    if cfile:
+        app.logger.error('Configuration could not be loaded from "%s"', cfile)
 
 # CORS support
 app.config['CORS_HEADERS'] = ['Content-Type', 'X-Requested-With']
